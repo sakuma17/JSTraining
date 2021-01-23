5d0045e4119c55b58ac296b4251276b04b7790aa
sakuma17 <ids08663@gmail.com>
2021-01-23 14:01:43 +0900

    jst

---
+++ 0123/cardgame/js/main.js
@@ -0,0 +1,85 @@
+'use strict';
+window.onload=function(){
+	class Card{
+		constructor(suit,num){
+			this.suit=suit;
+			this.num=num;
+			this.front=`${this.suit}${this.num<10?'0':''}${this.num}.gif`;
+		}
+	}
+	const cards=[];
+	const suits=['s','d','h','c'];
+	for(let i=0;i<suits.length;i++){
+		for(let j=1;j<=13;j++){
+			let card=new Card(suits[i],j);
+			cards.push(card);
+		}
+	}
+
+	shuffle();
+
+	const table=document.getElementById('table');
+	for(let i=0;i<suits.length;i++){
+		let tr=document.createElement('tr');
+		for(let j=0;j<13;j++){
+			let td=document.createElement('td');
+			let tempCard=cards[i*13+j];
+			td.classList.add('card','back');
+			td.onclick=flip;
+			td.style.backgroundImage=`url(images/${tempCard.front})`;
+			td.num=tempCard.num;
+			//td.textContent=`${tempCard.suit}:${tempCard.num}`;
+			tr.appendChild(td);
+		}
+		table.appendChild(tr);
+	}
+
+	function shuffle(){
+		let i=cards.length;
+		while(i){
+			let index=Math.floor(Math.random()*i--);
+			let temp=cards[index];
+			cards[index]=cards[i];
+			cards[i]=temp;
+		}
+	}
+
+	let firstCard=null;
+	let rest=52;
+	let flipTimerId=NaN;
+	let timerId=NaN;
+	function flip(eve){
+		let td=eve.target;
+		//td.classList.toggle('back');
+		if(!td.classList.contains('back') || flipTimerId){
+			return;
+		}
+		td.classList.remove('back');
+		if(firstCard==null){
+			firstCard=td;
+		}else{
+			if(firstCard.num===td.num){
+				rest-=2;
+				if(rest===0){
+					clearInterval(timerId);
+				}
+				firstCard.classList.add('fadeOut');
+				td.classList.add('fadeOut');
+				firstCard=null;
+			}else{
+				flipTimerId=setTimeout(function(){
+					firstCard.classList.add('back');
+					td.classList.add('back');
+					flipTimerId=NaN;
+					firstCard=null;
+				},1200)
+			}
+
+		}
+	}
+	const time=document.getElementById('time');
+	let sec=0;
+	timerId=setInterval(function(){
+		time.textContent=++sec;
+	},1000);
+};
